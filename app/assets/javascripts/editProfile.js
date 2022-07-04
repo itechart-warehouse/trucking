a3ad6c4893e521f43cab29f0f3d04d5bf113eb89
Kath Veremeychik <kateiiverem@gmail.com>
2022-07-04 17:04:04 +0300

    Feature/L-51. User profile. Add profile page form

---
+++ app/assets/javascripts/editProfile.js
@@ -0,0 +1,5 @@
+function editProfile() {
+    document.getElementById("myText").disabled = false;
+    document.getElementById("changePass").style.visibility = show;
+    document.getElementById("myText").focus();
+}
